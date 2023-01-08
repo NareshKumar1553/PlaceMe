75c28920544aa7e226c8eae30bba3604232d0e4d
NareshKumar1553 <nareshkumar.s1553@gmail.com>
2023-01-08 21:23:03 +0530

    commit

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import Details from './lib/Details';
 import ListDis from './lib/ListDis';
 import Login from './lib/Login';
 import MainPage from './lib/MainPage';
